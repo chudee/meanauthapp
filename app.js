750d85f5626074b1025347934bcfca17a7173eb7
chudee <geonwoo.chu@gmail.com>
2017-11-30 14:26:22 +0900

    project init

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const path = require('path')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const bluebird = require('bluebird')
+const config = require('./config')
+
+const app = express()
+
+const users = require('./routes/users');
+
+const port = 3000
+
+// MongoDb Setting & Connection
+mongoose.Promise = bluebird
+mongoose.connect(config.database, {useMongoClient: true})
+    .then(() => {console.log('Succesfully Connected to the Mongodb Database')})
+    .catch(() => {console.log('Error Connecting to the Mongodb Database')})
+
+// Middleware setting
+app.use(cors())
+app.use(bodyParser.json())
+
+// Set Static Foler
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes setting
+app.use('/users', users)
+
+// Index router
+app.get('/', (req, res) => {
+    res.send('express')
+})
+
+// Server start
+app.listen(port, () => {
+    console.log('express server is running: ' + port)
+})
---
+++ readme.md
@@ -0,0 +1,21 @@
+# MEAN AUTHENTICATION APP
+
+## About
+MEAN stack을 이용한 사용자인증 미니프로젝트 입니다.
+
+## Features
+- Authentication (Sign Up / Sign In)
+- ~~Dashboard~~
+- ~~User Search~~
+
+~~Preview: https://asdjasndkjasdjaskd/~~
+
+## Stack
+- mongoose
+- bluebird
+- Express
+- Angular 5
+- JsonWebToken
+- passport
+- passport-jwt
+- cors
